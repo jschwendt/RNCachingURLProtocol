d2fd35058ed4bf56e1d51c00521da5e5d22977f7
John Arnold <john@islandofdoom.com>
2013-02-22 14:31:21 -0400

    use SHA1 for hash key instead of nsobject hash

--- RNCachingURLProtocol.m
+++ RNCachingURLProtocol.m
@@ -27,6 +27,7 @@
 
 #import "RNCachingURLProtocol.h"
 #import "Reachability.h"
+#import <CommonCrypto/CommonCrypto.h>
 
 @interface NSURLRequest (MutableCopyWorkaround)
 
@@ -189,7 +190,21 @@ static RNCacheListStore *_bundleCacheListStore = nil;
 }
 
 + (NSString*)cacheKeyForString:(NSString*)urlString {
-    return [NSString stringWithFormat:@"%x", [urlString hash]];
+    
+    // SHA-1
+    const char *cStr = [urlString UTF8String];
+    unsigned char result[CC_SHA1_DIGEST_LENGTH];
+    CC_SHA1(cStr, strlen(cStr), result);
+    NSString *s = [NSString  stringWithFormat:
+                   @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+                   result[0], result[1], result[2], result[3], result[4],
+                   result[5], result[6], result[7],
+                   result[8], result[9], result[10], result[11], result[12],
+                   result[13], result[14], result[15],
+                   result[16], result[17], result[18], result[19]
+                   ];
+    
+    return s;
 }
 
 + (NSString *)cachePathForKey:(NSString *)key useBundle:(BOOL)useBundle {
